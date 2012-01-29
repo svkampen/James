ad0b33a82bb56e3a1fc5f7d597b048e10b622118
Sam van Kampen <samint64@gmail.com>
2012-01-29 16:49:52 +0100

    Updated, with more nicety.

--- ruinscript.py
+++ ruinscript.py
@@ -1,7 +1,9 @@
 __module_name__ = 'RUINScripts - RUINBot'
 __module_version__ = '1.01' 
 __module_description__ = 'RUIN Scripts for xChat =)'
+
 #All Message Scripts (w00t?)
+
 def message_scripts(word, word_eol, userdata):
     message=word[1]
     user=word[0]
@@ -18,34 +20,48 @@ def message_scripts(word, word_eol, userdata):
     stayinmsg = re.compile("@haha")
     sayhimsg = re.compile("@hi")
     vipmsg = re.compile("@site")
+
     #COMMANDS!
 
+    
     if vipmsg.search(message) != None:
         xchat.command("say Site: http://ruincommunity.net!")
+    
     if cmdmsg.search(message) != None:
         xchat.command("say Commands: [@status], [@soup], [@vibrate], [@suicide], [@about], [@tickle], [@noodles], [@guest], [@resurrect], [@haha], [@site]")
+    
     if statusmsg.search(message) != None:
         xchat.command("say Status: Online")
+    
     if noodlemsg.search(message) != None:
         xchat.command("me gives " + user + " ramen noodles.")
+    
     if soupmsg.search(message) != None:
         xchat.command("me gives " + user + " some chicken soup. Hmm, tasty!")
+    
     if vibrmsg.search(message) != None:
         xchat.command("me vibrates heavily!")
+    
     if ticklemsg.search(message) != None:
         xchat.command("me tickles " + user + " until they cum =P")
+    
     if aboutmsg.search(message) != None:
         xchat.command("say Most of the commands stolen from Caboodle. by svkampen. Free to look at code but it sucks, LOL.")
+    
     if suicidemsg.search(message) != None:
         xchat.command("me jumps off a cliff and grabs " + user + " whilst falling. They both die =3")
         xchat.command("nick RUINBot[DEAD]")
+    
     if resurrectmsg.search(message) != None:
         xchat.command("me resurrects himself...")
         xchat.command("nick RUINBot")
+    
     if guestmsg.search(message) != None:
         xchat.command("say Welcome to RUIN! Please read the signs at spawn to be promoted!")
+    
     if stayinmsg.search(message) != None:
         xchat.command("say Ha. Ha. Ha. Ha. Stayin' Alive!")
+    
     if sayhimsg.search(message) != None:
         xchat.command("say Hi!")
 
