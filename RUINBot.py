c3d4aae730a1ea3acf78f34c9bd398cde08a15ae
Sam van Kampen <samint64@gmail.com>
2012-02-01 16:29:22 +0400

    Added databases! And factoid command.

--- RUINBot.py
+++ RUINBot.py
@@ -31,7 +31,7 @@ def admin_only(f):
                 if nick == 'svkampen!svkampen@Srs.Face':
                         return f(self, nick, chan, arg)
                 elif nick == 'Nagah!max@Acael.us':
-                	return self._msg(chan, "NO JOIN FOR YU!")
+                	return self._msg(chan, "NO ADMEEN FOR YU!")
                 else:
                         return self._msg(chan, "Permission Denied.")
         return wrapper
@@ -70,7 +70,7 @@ class RUINHandler(DefaultCommandHandler):
     def cmdparse(self, nick, chan, msg):
         # Match the cmdchar or RUINBot's nick (@Amber I love you. This is epic. I love your code =)
         cmd_inmsg = re.compile(r"^(?:%s:\s+|%s)(\w+)(?:\s+(.*))?$" % ( self.client.nick, re.escape(config['cmdchar']), ))
-        
+
         m = cmd_inmsg.match(msg)
         if m:
             cmd = m.group(1)
@@ -86,25 +86,58 @@ class RUINHandler(DefaultCommandHandler):
 
     # Commandohs.
     def cmd_ABOUT(self, nick, chan, arg):
-        """about - Provides bot information."""
         self._msg(chan, "Ohai! I am RUINBot. Some/Most of my code is forked off Aiiane (Amber Yust)'s Kitn, but we shall be using own code soon =)")
         self._msg(chan, "My owner is svkampen. For more info, go to https://github.com/svkampen/RUINBot/wiki/About")
 
     @admin_only
     def cmd_JOIN(self, nick, chan, arg):
-        """Join - Join channels (admin-only)"""
+	usage = lambda: self._msg(chan, "Usage: join <chan>")
+        
         self._msg(chan, "Joining channel %s on request of %s." % (arg, nick))
         helpers.join(self.client, arg)
         logging.info("[JOIN] %s by %s" % (arg, nick))
+
     @admin_only
     def cmd_PART(self, nick, chan, arg):
-        """Part - Part channels (admin-only)"""
+        usage = lambda: self._msg(chan, "Usage: part <chan>")
         self._msg(chan, "Parting channel %s on request of %s." % (arg, nick))
         helpers.part(self.client, arg)
         logging.info("[PART] %s by %s" % (arg, nick))
 
+    def cmd_FACT
+	usage = lambda: self._msg(chan, "Usage: fact <operation> <trigger> <factoid> | Adds factoids =)")	
+
+	args = arg.split()
+        
+	global id = 0
+
+	if args[0] == "add":
+
+            id = id++
+	    word = args[1]
+	    factoid = ' '.join(args[2:])
+
+
+            db.execute("INSERT INTO mediarefs VALUES('%s', '%s', '%s')"" % (id, word, factoid)
+	    self._msg(chan, "%s: MediaReference Added =) (ID: %s)" % (nick, id))
+
+	    logging.info("[MediaRefs] ADDED REFERENCE %s | %s (Nick: %s)" % (word, factoid, nick))
+
+	if args[0] == "list":
+	    
+	    db.execute("SELECT * FROM mediarefs")
+	    self._msg(chan, db.fetchall())
+	    
+	    logging.info("[MediaRefs] LISTED!")
+
+	if args[0] == "listnr":
+	    
+	    db.execute("SELECT * FROM mediarefs WHERE ID ='%s'"" % (id))
+	    self._msg(chan, db.fetchall())
+	    
+	    logging.info("[MediaRefs] Listed %s" %s id)
+
     def cmd_TEEHEE(self, nick, chan, arg):
-        """@Haha ported."""
 	self._msg(chan, "Ha. Ha. Ha. Ha. Stayin' Alive!")
         logging.info("[CMDS] Executed.")
 
@@ -113,16 +146,23 @@ class RUINHandler(DefaultCommandHandler):
         self._msg(chan, "Donate: http://ruincommunity.net/donate")
 
     def cmd_CMDS(self, nick, chan, arg):
-	"""CMDS - Commands"""
 	self._msg(chan, "Commands: [@join]*, [@part]*, [@about], [@teehee], [@ruinsite]")
         self._msg(chan, "* = Owner Only")
 
     def _msg(self, chan, msg):
         helpers.msg(self.client, chan, msg)
+
 if __name__ == '__main__':
 	
         with open('config.yaml') as f:
 		config = yaml.safe_load(f)
+	
+	db = sqlite3.connect(config['db']['path'])
+        db.execute("""CREATE TABLE IF NOT EXISTS mediarefs (id INTEGER, trigger TEXT, factoid TEXT)""")
+	db.commit()
+	
+	
+
 	app = IRCApp()
 	clients = {}
 	for server, conf in config['servers'].iteritems():
