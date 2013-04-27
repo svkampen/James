0d5a95eaa71c250bb0ce610d28f3943f0fc632cf
svkampen <sam@tehsvk.net>
2013-04-27 15:02:04 +0200

    Empty master.

--- util/bin.py
+++
@@ -1,7 +0,0 @@
-def binencode(string):
-    return "".join('{:08b}'.format(ord(c)) for c in string)
-
-def bindecode(string):
-    ascii = [int(string[i:i+8], 2) for i in xrange(0, len(string), 8)]
-    charray = [chr(i) for i in ascii]
-    return ''.join(charray)
