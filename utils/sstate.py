8c9a95ff283b1aefb90049b298918a3a99dd6d2c
Tony Olagbaiye <frony0@gmail.com>
2013-06-03 21:05:52 +0100

    These should be in config

--- utils/sstate.py
+++ utils/sstate.py
@@ -48,7 +48,7 @@ class ServerState(object):
     """ A class that holds the active channels and admins and some more things about the bot that are server-specific. """
     def __init__(self):
         self.channels = []
-        self.admins = ['svkampen', 'lion', 'kvasir']
+        self.admins = []
         self.nick = 'JamesNext'
         self.notices = []
     
