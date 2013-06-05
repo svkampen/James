f749004264b03a8c5067f4485dadeaab8c8ac1cf
Tony Olagbaiye <frony0@gmail.com>
2013-06-05 12:03:52 +0100

    Fix name bug

--- bot.py
+++ bot.py
@@ -103,10 +103,11 @@ class James(IRCHandler):
         if chan == self.state.nick:
             chan = nick
         msg = msg['arg'].split(' ', 1)[1][1:]
-        target = msg.split(':')[0]
-        if target in self.lastmsgof.keys():
-            msg = msg.split(':')[1][1:]
-            nick = target
+        if msg.contains(':'):
+            target = msg.split(':')[0]
+            if target in self.lastmsgof.keys():
+                msg = msg.split(':', 1)[1].lstrip()
+                nick = target
         self.log.log(u"[%s] <%s> %s" % (chan, nick, msg))
         if msg.startswith('s/'):
             if msg.count('/') == 3 and not '$' in msg:
