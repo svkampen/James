8a8e029caec6c01362868876361e471e4420e05c
Tony Olagbaiye <frony0@gmail.com>
2013-06-05 15:25:43 +0100

    Fix weather

--- plugins/weather.py
+++ plugins/weather.py
@@ -6,7 +6,7 @@ Weather module.
 from .util.decorators import command
 import requests
 from .util.data import www_headers as headers
-from threading import Thread
+from threading import Thread, Event
 
 @command('weather')
 def weather(bot, nick, chan, arg):
@@ -14,32 +14,50 @@ def weather(bot, nick, chan, arg):
     if args[0].startswith("@"):
         nick = args[0][1:]
         args = args[1:]
-    locn = ' '.join(args[1:])
-    url = "http://api.openweathermap.org/data/2.5/find?units=metric&mode=json&type=like&q="
-    url += urllib.quote(locn, safe='')
+    locn = ' '.join(args)
+    url = "http://api.openweathermap.org/data/2.5/find"
+    params = {'units': 'metric', 'mode': 'json', 'type': 'like', 'q': locn}
     response = requests.get(url, headers=headers, params=params)
     data = response.json()
-    if data['count'] == 0:
-        return bot.msg(chan, "%s: Invalid location." % (nick))
     print(data)
+    if data['cod'] != '200' or data['count'] == 0:
+        return bot.msg(chan, "%s: Invalid location." % (nick))
     WeatherThread(bot, chan, data).start()
 
 class WeatherThread(Thread):
     def __init__(self, bot, chan, data):
         Thread.__init__(self)
-        this.bot = bot
-        this.chan = chan
-        this.data = data
+        self.bot = bot
+        self.chan = chan
+        self.data = data
 
     def run(self):
-        for entry in this.data['list']:
+        for entry in self.data['list']:
             name = u"%s, %s" % (entry['name'], entry['sys']['country'])
             main = entry['main']
-            temp = "Temperature is %d°C right now. Max: %d°C, Min: %d°C." % (main['temp'], main['temp_min'], main['temp_max'])
-            pressure = "Pressure is at %d hPa. %d hPa at Sea level, %d hPa at Ground level." % (main['pressure'], main['sea_level'], main['grnd_level'])
-            misc = "Humidity is %d%%. Wind going by %d° at %d m/s" % (main['humidity'], main['wind']['deg'], main['wind']['speed'])
-            feel = "%d%% cloudy, %s" % (main['clouds']['all'], main['weather']['description'])
-            this.bot.msg(this.chan, "%s: %s" % (name, temp))
-            this.bot.msg(this.chan, "%s: %s" % (name, pressure))
-            this.bot.msg(this.chan, "%s: %s" % (name, misc))
-            this.bot.msg(this.chan, "%s: %s" % (name, feel))
+            # This is nesty
+            try:
+                feel = "%d%% cloudy, %s" % (main['clouds']['all'], main['weather']['description'])
+            except:
+                try:
+                    feel = "%s" % (main['weather']['description'])
+                except:
+                    feel = ''
+            try:
+                temp = "Temperature is %d°C right now. Max: %d°C, Min: %d°C." % (main['temp'], main['temp_min'], main['temp_max'])
+            except:
+                try:
+                    temp = "Temperature is %d°C right now." % (main['temp'])
+                except:
+                    temp = ''
+            try:
+                misc = "Pressure is at %d hPa. Humidity is %d%%. Wind going by %d° at %d m/s" % (main['pressure'], main['humidity'], main['wind']['deg'], main['wind']['speed'])
+            except:
+                try:
+                    misc = "Wind going by %d° at %d m/s" % (main['wind']['deg'], main['wind']['speed'])
+                except:
+                    misc = ''
+            for line in (temp, misc, feel):
+                if line != '':
+                    self.bot.msg(self.chan, "%s: %s" % (name, line))
+            Event().wait(8)
