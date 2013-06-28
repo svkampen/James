3ad8be0bbf75bd5a30b39ebc05064ec7b211b310
Tony Olagbaiye <frony0@gmail.com>
2013-06-28 13:45:29 +0100

    wiki updates

--- plugins/uncyclopedia.py
+++ plugins/uncyclopedia.py
@@ -1,5 +1,5 @@
 """ 
-Wikipedia 'API' (using BeautifulSoup)
+Wikia 'API' (using BeautifulSoup)
 """
 from .util.decorators import command, initializer
 from bs4 import BeautifulSoup as soupify
@@ -15,29 +15,38 @@ def initialize_plugin(bot):
     """ Initialize this plugin. """
     bot.state.data['sentence_re'] = re.compile(r"((Dhr\.|Mrs\.|Mr\.)?(.*?)\.)")
 
-@command('wiki')
-def wikipedia_get_first_sentence(bot, nick, target, chan, arg):
-    """ Get the first sentence in a wikipedia article. """
+@command('wikia')
+def wikia_get_first_sentence(bot, nick, target, chan, arg):
+    """ Get the first sentence in a wikia article. """
     headers = {
         'User-Agent': 'Mozilla/5.0 (compatible) / JamesIRC'
     }
 
+    wiki = "uncyclopedia"
+    if arg.startswith("@"):
+        args = arg.split(" ")
+        wiki = args[0][1:]
+        arg = " ".join(args[1:])
     arg = arg.replace(" ", "_")
     arg = urlencode(arg)
 
-    url = 'http://en.wikipedia.org/wiki/%s?action=render' % (arg)
+    url = 'http://%s.wikia.com/wiki/%s?action=render' % (wiki,arg)
     response = requests.get(url, headers=headers)
 
     soup = soupify(response.text)
     for s in soup.findAll('table', {'class': 'infobox'}):
         s.extract()
     paragraphs = soup.findAll('p')
-    first_paragraph = paragraphs[0].getText()
+    i = 0
+    first_paragraph = None
+    while first_paragraph == None or len(first_paragraph) < 10:
+        first_paragraph = paragraphs[i].getText()
+        i += 1
     first_sentence = bot.state.data['sentence_re'].match("%s" % (first_paragraph))
     if first_sentence:
         first_sentence = first_sentence.groups()[0]
     else:
         if len(first_paragraph.split(". ")[0]) > 15:
-            bot._msg(chan, "%s: %s -- read more: %s" % (target, first_paragraph.split(". ")[0], bot.state.data['shortener'](bot, url)))
+            bot._msg(chan, "%s: %s -- read more: %s" % (nick, first_paragraph.split(". ")[0], bot.state.data['shortener'](bot, url)))
             return
-    bot._msg(chan, "%s: %s -- read more: %s" % (target, first_sentence, bot.state.data['shortener'](bot, url)))
+    bot._msg(chan, "%s: %s -- read more: %s" % (nick, first_sentence, bot.state.data['shortener'](bot, url)))
