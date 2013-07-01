708ad4b2d8f0e7fda71be1b8c20442f01b7c8f07
Tony Olagbaiye <frony0@gmail.com>
2013-07-01 18:26:50 +0100

    Fix history casebug

--- plugins/history.py
+++ plugins/history.py
@@ -11,7 +11,7 @@ def history(bot, nick, target, chan, arg):
     try:
         if len(args) > 2 or len(args) < 2 or int(args[1]) > 16 or int(args[1]) < 1:
             return bot.msg(chan, "Usage: +history <nick> <1-16>")
-        victim = args[0]
+        victim = args[0].lower()
         num = int(args[1])
         bot.msg(chan, "\n".join(reversed(["<" + victim + "> " + x for x in bot.lastmsgof[chan][victim]][0:num])))
     except:
