6dc61200ba4272a482c32134e268dc70882adeb6
Sam van Kampen <sam@tehsvk.net>
2013-07-13 12:25:53 +0200

    And again

--- plugins/mute.py
+++ plugins/mute.py
@@ -7,7 +7,7 @@ from .util.decorators import command, require_admin
 @require_admin
 @command('mute', category='meta')
 def mute(self, nick, target, chan, arg):
-	""" Prohibit someone from using the bot. Admin-only. """
+    """ Prohibit someone from using the bot. Admin-only. """
     self.state.mute(arg)
     self.msg(chan, "%s: You are temporarily prohibited from using this bot" % (arg))
 
@@ -15,6 +15,6 @@ def mute(self, nick, target, chan, arg):
 @require_admin
 @command('unmute', category='meta')
 def unmute(self, nick, target, chan, arg):
-	""" Let someone use the bot. Admin-only. """
+    """ Let someone use the bot. Admin-only. """
     self.state.unmute(arg)
     self.msg(chan, "%s: You are now allowed to use this bot" % (arg))
