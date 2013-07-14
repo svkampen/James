1a3716b8ca8146807c6a92c34d1118dfe2396d5f
Sam van Kampen <sam@tehsvk.net>
2013-07-14 11:39:21 +0200

    Cut out more crap

--- utils/parse.py
+++ utils/parse.py
@@ -48,33 +48,3 @@ def inline_python(bot, nick, chan, msg):
         except BaseException:
             traceback.print_exc()
     return msg.replace('`', '')
-
-def check_for_sed(bot, msg):
-    """ Check whether a message contains a (valid) sed statement """
-    import re
-    if re.match("^(\w+: )?s/.+/.+(/([gi]?){2})?$", msg):
-        return True
-
-def parse_sed(bot, sedmsg, oldmsgs):
-    """ Parse a sed snippet """
-    import traceback
-    import re
-    split_msg = sedmsg.split('/')[1:]
-    glob = False
-    case = False
-    if len(split_msg) == 3:
-        if 'g' in split_msg[2]:
-            glob = True
-        if 'i' in split_msg[2]:
-            case = True
-    try:
-        regex = re.compile(split_msg[0], re.I if case else 0)
-        for msg in oldmsgs:
-            if regex.search(msg) is not None:
-                if case:
-                    return {'to_replace': "(?i)"+split_msg[0], 'replacement': lambda match: split_msg[1].replace("&", match.group(0)), 'oldmsg': msg, 'glob': glob}
-                else:
-                    return {'to_replace': split_msg[0], 'replacement': lambda match: split_msg[1].replace("&", match.group(0)), 'oldmsg': msg, 'glob': glob}
-    except BaseException:
-        traceback.print_exc()
-    return -1
