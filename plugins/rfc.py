e5e0f467ee3b27cf458eb45827fe4afdd50a91e7
Sam van Kampen <sam@tehsvk.net>
2014-08-14 16:11:03 +0200

    Added rfc search, rfc numeric search, IRC RFC section search

--- plugins/rfc.py
+++ plugins/rfc.py
@@ -3,28 +3,43 @@ from .util.data import get_doc
 
 import difflib
 
+
+rfc_url = "http://tools.ietf.org/html/"
 rfcdata = {}
+rfcnumdata = None
+rfcs = {}
 sections = None
+rfcnames = None
 matcher = difflib.SequenceMatcher(None)
 
 @initializer
 def plugin_init(bot):
-    global sections
+    global sections, rfcnumdata, rfcnames
     f = open("data/rfc.data", "r").readlines()
     for line in f:
         url, title = line.strip().split(" ", 1)
         rfcdata[title] = url
     sections = list(rfcdata.keys())
+    line = open("data/rfcnum.data", "r").readline()
+    rfcnumdata = eval(line.strip())
+    f = open("data/rfcs.data", "r").readlines()
+    for line in f:
+        num = line.split()[0]
+        title = line.split(" ", 1)[1].strip()
+        rfcs[title] = num
+    rfcnames = list(rfcs.keys())
 
-def fmatch(title, nmatches=2):
+
+def fmatch(title, items=None, nmatches=2):
     matches = {}
-    for item in sections:
+    items = items or sections
+    for item in items:
         matcher.set_seqs(title.lower(), item.lower())
         matches[item] = matcher.ratio()
     matches_list = sorted(list(matches.items()), key=lambda x: -x[1])
     return matches_list[:nmatches]
 
-@command('rfcmatch', category='misc')
+@command('rfc', category='rfc')
 def rfcmatch(bot, nick, chan, arg):
     """ rfcmatch <item> <n> -> return 1 or <n> RFC section(s) most matching the input string """
     if not arg:
@@ -33,7 +48,24 @@ def rfcmatch(bot, nick, chan, arg):
         n = int(arg.split()[-1])
     else:
         n = 1
-    matches = fmatch(arg, 2)
-    mmatches = fmatch(arg+' message', 2)
+    matches = fmatch(arg, nmatches=2)
+    mmatches = fmatch(arg+' message', nmatches=2)
     combined = sorted(matches+mmatches, key=lambda x: -x[1])
     return [bot.msg(chan, "%s -> %s" % (bot.hicolor(i[0]), rfcdata[i[0]])) for i in combined[:n]]
+
+@command('rfc.num', 'rfc.numeric', category='rfc')
+def rfcnum(bot, nick, chan, arg):
+    """ rfc.num <numeric> -> return data on an RFC numeric """
+    if not arg:
+        return bot.msg(chan, get_doc())
+    if arg not in rfcnumdata.keys():
+        return bot.msg(chan, "%s: %s" % (bot.hicolor(arg), bot.defaultcolor("unknown numeric")))
+    bot.msg(chan, rfcnumdata[arg])
+
+@command('rfc.find', category='rfc')
+def rfcfind(bot, nick, chan, arg):
+    """ rfcfind <name> -> return the RFC URL by name """
+    if not arg:
+        return bot.msg(chan, get_doc())
+    matches = fmatch(arg, rfcnames)
+    return [bot.msg(chan, "%s -> %s%s" % (bot.hicolor(i[0]), rfc_url, rfcs[i[0]])) for i in matches]
