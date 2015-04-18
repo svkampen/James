3e2f210531a621f5df6c0ac48a3277ed162564d3
Sam van Kampen <sam@tehsvk.net>
2015-04-18 12:30:48 +0000

    Remove evaluate function

--- utils/parse.py
+++ utils/parse.py
@@ -2,13 +2,6 @@
 IRC Parser - parse.py
 """
 
-import inspect
-import traceback
-import re
-import codecs
-import subprocess
-import sys
-
 def parse(msg):
     """ Parse an IRC protocol message """
     if msg.startswith("PING"):
@@ -18,26 +11,4 @@ def parse(msg):
         info = {"method": splitmsg[1], "host": splitmsg[0][1:], "arg":
                 splitmsg[2]}
     return info
-
-def evaluate(self, nick, chan, msg):
-    """ Evaluate python code. """
-    try:
-        output = eval(msg, globals(), locals())
-        if output is not None:
-            self.leo = output
-            if type(self.leo) == tuple:
-                self.leo = list(self.leo)
-                output = list(output)
-            return output
-    except (NameError, SyntaxError):
-        try:
-            exec(msg, globals())
-        except:
-            try:
-                exec(msg, locals())
-            except:
-                try:
-                    exec(msg,globals(),locals())
-                except:
-                    exec(msg,locals(),globals())
 
