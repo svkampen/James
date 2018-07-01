9a72171ce6b74116f978bfe635d44679e12e6fd8
Sam van Kampen <sam@tehsvk.net>
2018-07-01 01:56:35 +0000

    Add github commit display

--- plugins/misc.py
+++ plugins/misc.py
@@ -7,9 +7,11 @@ from .util.data import get_doc
 import datetime
 import random
 import re
+import requests
 from collections import namedtuple
 
 Quote = namedtuple("Quote", ["quote", "author", "extra"])
+GITHUBURL_RE = r"(?:https?:\/\/)github.com\/([^\/]+)\/([^\/]+)\/commit\/([a-f0-9]+)"
 
 class QuoteFile(object):
     def __init__(self, quotedb):
@@ -59,4 +61,33 @@ def unicode_please(bot, nick, chan, arg):
     endpos = startpos + arg
     bot.msg(chan, "%s" % ("".join([chr(i) for i in range(startpos, endpos)])))
 
+@initializer
+def init_plugin(bot):
+    bot.state.events.MessageEvent.register(github_url)
+
+def github_url(bot, nick, chan, msg):
+    match = re.search(GITHUBURL_RE, msg)
+    if not match:
+        return
+
+    user, repo, sha = match.groups()
+
+    response = requests.get("https://api.github.com/repos/%s/%s/commits/%s" % (user, repo, sha))
+    if (response.status_code != 200):
+        return
+
+    data = response.json()
+    commit = data['commit']
+    
+    # Format: [sha] user shortlog +add -sub
+    user = commit['committer']['name']
+    shortlog = commit['message'].split('\n')[0]
+    stats = data['stats']
+
+    bot.msg(chan, "%s %s %s %s %s" %
+            (bot.style.color("[%s]" % sha[:7], "gray"),
+             bot.style.color(user, "blue"),
+             bot.style.color(shortlog, "lblue"),
+             bot.style.color("+%s" % (stats['additions']), "green"),
+             bot.style.color("-%s" % (stats['deletions']), "red")))
 
